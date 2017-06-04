b89aaa01ad5f95bd45f7cd3fc2a40ec78fc078ed
Samir Medjdoub <contact@codeconcept.fr>
2017-06-04 17:13:21 +0200

    adding twitter bootstrap

--- src/app/job-list/job-list.component.ts
+++ src/app/job-list/job-list.component.ts
@@ -8,12 +8,16 @@ import 'rxjs/Add/operator/map';
   styleUrls: ['./job-list.component.css']
 })
 export class JobListComponent implements OnInit {
+  jobs = [];
 
   constructor(private http: Http) { }
 
   ngOnInit() {
     this.http.get('data/jobs.json')
-              .map(res => console.log(res.json()))
+              .map(res => {
+                console.log(res.json());
+                this.jobs = res.json();
+              })
               .subscribe();
   }
 
