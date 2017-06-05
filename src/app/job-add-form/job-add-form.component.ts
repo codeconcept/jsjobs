0fc07e48b204545b2ff72de1ac7bd3dfc480398f
Samir Medjdoub <contact@codeconcept.fr>
2017-06-05 15:16:36 +0200

    generating form + radio button group

--- src/app/job-add-form/job-add-form.component.ts
+++ src/app/job-add-form/job-add-form.component.ts
@@ -18,6 +18,31 @@ export class JobAddFormComponent implements OnInit {
     { id: 5, name: 'indépendant', value: 'freelance' }
   ]
 
+  currencies = [
+    {id: 1, name: 'euros', value: 'EU', symbol: '€'},
+    {id: 2, name: 'livres sterling', value: 'POUNDS', symbol: '£'},
+    {id: 3, name: 'francs CFA', value: 'CFA', symbol: 'CFA'},
+    {id: 4, name: 'dollars canadien', value: 'CAD', symbol: '$'}
+  ];
+
+  statuses = [
+    {id: 1, name: 'cadre', value: 'executive'},
+    {id: 1, name: 'employé', value: 'employee'}
+  ];
+
+  experience = [
+    { id: 1, name: 'junior', value: 'junior'},
+    { id: 2, name: 'medior', value: 'medior'},
+    { id: 3, name: 'senior', value: 'senior'}
+  ];
+
+  areas = [
+    {id: 1, name: 'aucun déplacements', value: 'none'},
+    {id: 2, name: 'déplacements régionaux', value: 'region'},
+    {id: 3, name: 'déplacements nationaux', value: 'nation'},
+    {id: 4, name: 'déplacements internationaux', value: 'international'}
+  ];
+
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
@@ -29,13 +54,13 @@ export class JobAddFormComponent implements OnInit {
       zipcode: 35,
       description: '',
       contract: '',
-      salary: 0,
-      currency: 'euros',
-      startdate: new Date(),
+      salary: null,
+      currency: '',
       experience: '',
       status: '',
       area: '',
       field: '',
+      startdate: new Date(),
       publishdate: new Date(),
       lastupdate: new Date()
     })
